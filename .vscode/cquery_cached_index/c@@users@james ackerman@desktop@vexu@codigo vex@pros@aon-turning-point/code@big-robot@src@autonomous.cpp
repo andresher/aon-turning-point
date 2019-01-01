c63a0377843fa741881c311d4553d09817aba7c1
James-Ackerman <james.ackerman@upr.edu>
2019-01-01 13:33:56 -0400

    Finished big-robot auton prototype Changed many comments and TODOS Next up should be line tracker and vision sensor code.

--- .vscode/cquery_cached_index/c@@users@james ackerman@desktop@vexu@codigo vex@pros@aon-turning-point/code@big-robot@src@autonomous.cpp
+++ .vscode/cquery_cached_index/c@@users@james ackerman@desktop@vexu@codigo vex@pros@aon-turning-point/code@big-robot@src@autonomous.cpp
@@ -76,6 +76,8 @@
 
 
 // Write autonomous code here
+//TODO: verify if you can use non-async functions with async controllers, if not, fix code
+//TODO: try and place all the motor definitions and stuff in
 //TODO: measure degrees needed for 1 "punch" of puncher, probably 300-360 or so
 //TODO: line tracker code task running continuously
 //TODO: Vision Sensor code
@@ -83,68 +85,99 @@
 //TODO: Verify clockwise and counter-clockwise syntax for okapi motor controllers
 //TODO: Chekc if lift maintains its height when raised
 //TODO: Verify if these controller values are accumulative or they reset to 0 after every use
-////////////////////////////////////AUTONOMOUS///////////////////////////////////////////////////
+
 //TODO: Find actual low and high goal values in degrees (TUNING)
 const int STARTING_HEIGHT = 0;
 const int LOW_GOAL_HEIGHT = 53;   //low goal is 69% of high goal more or less but this is in degrees so idk
 const int HIGH_GOAL_HEIGHT = 75;
+////////////////////////////////////AUTONOMOUS///////////////////////////////////////////////////
 void autonomous() {
 
   //Assuming controller values reset to 0 after every use
   //Autonomous routine for 24x24 RED
+  //PART 1: SHOOT PRELOAD AND BALL, SCORE BACK LOW GOAL CAP
   puncherController.setTarget(300_deg);       //Shoot ball to top center flag
   puncherController.waitUntilSettled();
-  driveController.turnAngle(35_deg);         //Turn towards same color cap //I assume this is clockwise
+  driveController.turnAngle(35_deg);          //Turn towards same color cap //I assume this is clockwise
   driveController.waitUntilSettled();
   intakeController.setTarget(200_rpm);        //Chupa intake
   conveyorController.setTarget(200_rpm);      //Sube bola al puncher
-  driveController.moveDistance(4_ft);         //Move towards same color cap
-  driveController.waitUntilSettled();
-  pros::delay(600);                          //Wait for robot to intake ball and cap securely
-  intakeController.setTarget(30_rpm);         //Use intake to hold cap
-  //TODO: REPLACE THIS TURN WITH VISION SENSOR CODE
-  driveController.turnAngle(-80_deg);          //Turn facing middle center flag //I assume this is clockwise
-  puncherController.setTarget(300_deg);       //Shoot ball to middle center flag
-  puncherController.waitUntilSettled();
-  liftController.setTarget(LOW_GOAL_HEIGHT);  //Raise lift to low goal height
-  driveController.turnAngle(80_deg);         //Turn clockwise face opposite team's side
-  puncherController.waitUntilSettled();
-  driveController.moveDistance(-2_ft);        //move backwards until "horizontally" alligned with back low goal post
-  driveController.waitUntilSettled();
-  driveController.turnAngle(80_deg);         //Turn clockwise to face back low goal
-  driveController.moveDistance(1_ft);         //Drive towards back low goal
-  pros::delay(300)                            //wait for robot to stop jiggling
-  liftController.setTarget(-10_deg);  //Raise lower lift to insert cap on post
-  intakeController.setTarget(-100_rpm);       //Reverse intake to release cap
-  driveController.moveDistance(-0.5_ft);      //Drive in reverse away from cap on goal
-  driveController.waitUntilSettled();
-  intakeController.setTarget(0_rpm);
-  liftController.setTarget(-(LOW_GOAL_HEIGHT-10));  //Return lift to starting position
-  driveController.turnAngle(-90_deg);          //Turn 90 degrees counter-clockwise to face opponent-colored cap
-  driveController.waitUntilSettled();
-  intakeController.setTarget(200_rpm);        //Chupa intake
-  conveyorController.setTarget(200_rpm);      //Sube bola al puncher
-  driveController.moveDistance(4_ft);         //Move towards same color cap
+  driveController.moveDistanceAsync(4_ft);    //Move towards same color cap
   driveController.waitUntilSettled();
   pros::delay(600);                           //Wait for robot to intake ball and cap securely
   intakeController.setTarget(30_rpm);         //Use intake to hold cap
-  liftController.setTarget(LOW_GOAL_HEIGHT);  //Raise lift to low goal height
-  driveController.turnAngle(-120_deg);         //Turn clockwise face opposite team's side
+  //TODO: REPLACE THIS TURN WITH VISION SENSOR CODE
+  driveController.turnAngle(-80_deg);         //Turn facing middle center flag //I assume this is clockwise
+  puncherController.setTarget(300_deg);       //Shoot ball to middle center flag
   puncherController.waitUntilSettled();
-  rotatorController.setTarget(180_deg);       //Flip cap
-  driveController.setTarget(115_deg);         //Turn 115 degrees clockwise towards side low goal
+  liftController.setTarget(LOW_GOAL_HEIGHT);  //Raise lift to low goal height
+  driveController.turnAngle(80_deg);          //Turn clockwise face opposite team's side
+  puncherController.waitUntilSettled();
+  driveController.moveDistanceAsync(-2_ft);   //move backwards until "horizontally" alligned with back low goal post
+  driveController.waitUntilSettled();
+  driveController.turnAngle(80_deg);          //Turn clockwise to face back low goal
+  driveController.moveDistanceAsync(1_ft);    //Drive towards back low goal
+  pros::delay(300);                           //wait for robot to stop jiggling
+  liftController.setTarget(-10_deg);          //Raise lower lift to insert cap on post
+  intakeController.setTarget(-100_rpm);       //Reverse intake to release cap
+  driveController.moveDistanceAsync(-0.5_ft); //Drive in reverse away from cap on goal
+  driveController.waitUntilSettled();
+  intakeController.setTarget(0_rpm);
+  liftController.setTarget(-(LOW_GOAL_HEIGHT-10));  //Return lift to starting position
 
+  //PART 2: SCORE CAP ON SIDE LOW GOAL
 
-
-
-
-
-
-
+  driveController.turnAngle(-90_deg);         //Turn 90 degrees counter-clockwise to face opponent-colored cap
+  intakeController.setTarget(200_rpm);        //Chupa intake
+  conveyorController.setTarget(200_rpm);      //Sube bola al puncher
+  driveController.moveDistanceAsync(4_ft);    //Move towards same color cap
+  driveController.waitUntilSettled();
+  pros::delay(600);                           //Wait for robot to intake ball and cap securely
+  intakeController.setTarget(30_rpm);         //Use intake to hold cap
   //maintain lift at height
+  liftController.setTarget(LOW_GOAL_HEIGHT);  //Raise lift to low goal height
+  rotatorController.setTarget(180_deg);       //Flip cap
+  driveController.setTarget(-110_deg);        //Turn 110 degrees counter-clockwise towards slightly ahead of side low goal
+  driveController.moveDistanceAsync();        //Drive forwards towards slightly ahead of side low goal
+  driveController.setTarget(10_deg);          //Turn 10 degrees counter-clockwise to face side low goal
+  driveController.moveDistanceAsync(0.5_ft);  //Drive towards back low goal
+  pros::delay(300);                           //give robot some time to move
+  liftController.setTarget(-10_deg);          //Raise lower lift to insert cap on post
+  intakeController.setTarget(-100_rpm);       //Reverse intake to release cap
+  driveController.moveDistanceAsync(-0.5_ft); //Drive in reverse away from cap on goal
+  driveController.waitUntilSettled();
+  intakeController.setTarget(0_rpm);
+  liftController.setTarget(-(LOW_GOAL_HEIGHT-10));  //Return lift to starting position
 
+  //PART 3: SCORE CAP BROUGHT BY 15X15 ON HIGH GOAL
 
-  //
+  driveController.turnAngle(90_deg);           //Turn 90 degrees clockwise to face cap left by 15x15 robot
+  driveController.waitUntilSettled();
+  intakeController.setTarget(200_rpm);         //Chupa intake
+  driveController.moveDistanceAsync(4.5_ft);   //Move towards cap left by 15x15 robot
+  driveController.waitUntilSettled();
+  pros::delay(600);                            //Wait for robot to intake ball and cap securely
+  intakeController.setTarget(30_rpm);          //Use intake to hold cap
+  liftController.setTarget(HIGH_GOAL_HEIGHT);  //raise lift to high goal height
+  //Maybe use line trackers here?
+  driveController.moveDistanceAsync(-0.5_ft);  //drive backwards to allign center of robot with white line with white line
+  driveController.waitUntilSettled();
+  driveController.turnAngle(-90_deg);          //Turn 90 degrees counter-clockwise to face high goal post
+  driveController.moveDistanceAsync(0.5_ft);  //Drive towards back low goal
+  pros::delay(300);                           //give robot some time to move
+  liftController.setTarget(-10_deg);          //Raise lower lift to insert cap on post
+  intakeController.setTarget(-100_rpm);       //Reverse intake to release cap
+  driveController.moveDistanceAsync(-0.5_ft); //Drive in reverse away from cap on goal
+  driveController.waitUntilSettled();
+  intakeController.setTarget(0_rpm);
+  liftController.setTarget(-(HIGH_GOAL_HEIGHT-10));  //Return lift to starting position
+
+  //PART 4: RETURN TO BACK OF FIELD
+  driveController.turnAngle(70_deg);           //Turn robot on an angle so it can cut to the back of the field
+  driveController.moveDistanceAsync(-6_ft); //Drive in reverse away from cap on goal
+  driveController.waitUntilSettled();
+
+  //reference stuff
   // Motor intakeL(INTAKE_MOTOR_LEFT);        //motor on INTAKE_MOTOR_LEFT port
   // Motor intakeR(INTAKE_MOTOR_RIGHT, true); //motor on INTAKE_MOTOR_RIGHT port reversed driection
   // Motor conveyor(CONVEYOR_MOTOR);          //motor on CONVEYOR_MOTOR port
@@ -159,7 +192,7 @@ void autonomous() {
   // rotatorController.setTarget(180_deg);  //flip intake
   // conveyorController.setTarget(200_rpm); //move conveyor full speed
   // liftController.setTarget(50_deg);      //raise lift //TODO: measure distance per degree
-  // driveController.moveDistance(1_m);     // Move 1 meter to the first goal
+  // driveController.moveDistanceAsync(1_m);     // Move 1 meter to the first goal
   // driveController.turnAngle(90_deg);     // Turn 90 degrees
   //pros::delay(1000) or pros::Task::delay(1000) idk which one
 }
